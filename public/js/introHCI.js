0797b178a314d9e07540513713efb2434571cfbb
Jake Pham <jakepham90@gmail.com>
2014-02-14 00:04:12 -0800

    Add stuff

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -2,31 +2,56 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+  initializePage();
 })
+
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$('.project a').click(addProjectDetails);
+  $('.project a').click(addProjectDetails);
 
-	$('#colorBtn').click(randomizeColors);
+  $('#colorBtn').click(randomizeColors);
+
+  $("#testjs").click(function(e) {
+    $.get("http://URL", callBackFn);
+
+  });
 }
+
+
+function callBackFn (result) {
+  var projectHTML = '<a href="#" class ="thumbnail">' + 
+    '<img src="' + result['image'] + '" class="detailsImage">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] + '</small></p></a>'+
+    '<p>' + result['summary'] +'</p>';
+
+  $("#project" + result['id']).html(projectHTML);
+
+}
+
+
+//$.post("http://URL", {"json":"json"}, callBa
 
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
-	// Prevent following the link
-	e.preventDefault();
+  // Prevent following the link
+  e.preventDefault();
 
-	// Get the div ID, e.g., "project3"
-	var projectID = $(this).closest('.project').attr('id');
-	// get rid of 'project' from the front of the id 'project3'
-	var idNumber = projectID.substr('project'.length);
+  // Get the div ID, e.g., "project3"
+  var targetID = $(this).closest('.project').attr('id');
+  // get rid of 'project' from the front of the id 'project3'
+  var idValue = targetID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+  var targetURL = "/project/" +idValue);
+
+  $.get(targetURL, callBackFn); 
+
+
 }
 
 /*
@@ -34,5 +59,15 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
-	console.log("User clicked on color button");
+  $.get("/palette", colorCallBack);
 }
+
+function colorCallBack (result) {
+	var temp = result['colors'];
+	var colors = temp['hex'];
+  $('body').css('background-color', colors[0]);
+  $('.thumbnail').css('background-color', colors[1]);
+  $('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+  $('p').css('color', colors[3]);
+  $('.project img').css('opacity', .75);
+}'
